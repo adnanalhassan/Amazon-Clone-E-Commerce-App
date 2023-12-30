b9335d8b8d0222f37126e85027151e5a35139de2
Adnan Alhassan <adnanalhassan2019@gmail.com>
2023-12-30 20:52:53 +0000

    Final Changes

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,21 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCsOJQ6l4aFcWLWCYr8_-y4YUltoD1Wf4w",
+    authDomain: "clone-4e04b.firebaseapp.com",
+    projectId: "clone-4e04b",
+    storageBucket: "clone-4e04b.appspot.com",
+    messagingSenderId: "894534528736",
+    appId: "1:894534528736:web:0ea0dab457eb25335cea85",
+    measurementId: "G-QHJP2C025Q"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
